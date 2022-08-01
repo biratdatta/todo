98d28755f382e61bdf4f365d00972f0172bcd21e
biratdatta <birat.datta26@gmail.com>
2022-08-01 22:32:59 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { AiOutlinePlus } from "react-icons/ai";
 const style = {
  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
 
@@ -16,10 +17,12 @@ function App() {
 <input className={style.input}
 type= "text" placeholder='Add Todo'/>
 <button className={style.button}>
-
+<AiOutlinePlus size="30"/>
 </button>
 
   </form>
+
+  
  </div>
 
 
