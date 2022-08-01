d8f4ff3f6064370422a4c31ede5cd9ca7f148d84
biratdatta <birat.datta26@gmail.com>
2022-08-01 20:24:28 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+const style = {
+bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2f80ed] to-[#1CB5E0]`
+
+}
+
+
+
 function App() {
   return (
-    <div className="App">
+    <div className={StyleSheet.bg}>
  
     </div>
   );
