2762c3d9f70c91acecee7fda143283e9cf34c573
biratdatta <birat.datta26@gmail.com>
2022-08-02 23:06:16 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Todo from './Todo'
 
 const style = {
  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+ container: 'bg-slate-100 max-w-[500px] w-full m-auto rounded-md shsdow-xl p-5'
 
 }
 
